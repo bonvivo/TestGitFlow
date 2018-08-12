221ebf6b252c4e56430f0f54e3fe9b786b7c04e5
jaca <jacekzatyka@gmail.com>
2018-08-12 10:29:22 +0200

    Dodanie metody do wyswietlania tekstu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        wyswietlTekst( "TEST" );
+    }
+    public static void wyswietlTekst(String tekst) {
+        System.out.println("Tekst: " + tekst);
     }
 }
